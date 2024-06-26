185df6d9d7b188fa60235c08b57d7a75ea9595d3
zero <squiredd@gmail.com>
2024-06-25 20:53:58 -0700

    load input from file

--- source/SolverConsole/Program.cs
+++ source/SolverConsole/Program.cs
@@ -5,17 +5,8 @@ using Solver.Domain.Cell;
 
 Console.WriteLine("Hello, World!");
 
-var b = new GameBoard(
-    new SolvedCell(1), UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance,
-    UnsolvedCell.Instance, new SolvedCell(2), UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance,
-    UnsolvedCell.Instance, UnsolvedCell.Instance, new SolvedCell(3), UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance,
-    UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, new SolvedCell(4), UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance,
-    UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance,
-    UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance,
-    UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance,
-    UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance, UnsolvedCell.Instance,
-    new SolvedCell(2), new SolvedCell(1), new SolvedCell(9), new SolvedCell(8),new SolvedCell(5),new SolvedCell(6),new SolvedCell(7),new SolvedCell(4),new SolvedCell(3) 
-    );
+var builder = new BoardBuilder();
+var b = builder.CreateFrom9x9Csv("input.txt");
 
 for (int rowIndex = 0; rowIndex < b.Rows.Length; rowIndex++)
 {
