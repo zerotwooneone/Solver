8a5fab9637cbf3376a8685ecd919ce49c460a0d3
zero <squiredd@gmail.com>
2024-06-25 20:54:51 -0700

    cleanup

--- source/SolverConsole/Program.cs
+++ source/SolverConsole/Program.cs
@@ -8,19 +8,5 @@ Console.WriteLine("Hello, World!");
 var builder = new BoardBuilder();
 var b = builder.CreateFrom9x9Csv("input.txt");
 
-for (int rowIndex = 0; rowIndex < b.Rows.Length; rowIndex++)
-{
-    Console.WriteLine($"row {rowIndex} isValid/isSolved: {b.Rows[rowIndex].Check()}");
-}
-Console.WriteLine();
-for (int columnIndex = 0; columnIndex < b.Columns.Length; columnIndex++)
-{
-    Console.WriteLine($"column {columnIndex} isValid/isSolved: {b.Columns[columnIndex].Check()}");
-}
-Console.WriteLine();
-for (int regionIndex = 0; regionIndex < b.Regions.Length; regionIndex++)
-{
-    Console.WriteLine($"region {regionIndex} isValid/isSolved: {b.Regions[regionIndex].Check()}");
-}
 Console.WriteLine();
 Console.WriteLine($"isValid: {b.IsValid} isSolved:{b.IsSolved}");
