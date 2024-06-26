4c085cfe8abc2795dc90e3888e34808f9d4e0010
zero <squiredd@gmail.com>
2024-06-26 10:32:53 -0700

    add board solver tests

---
+++ source/Solver.Domain.Tests/Board/BoardSolverTests.cs
@@ -0,0 +1,18 @@
+﻿using Solver.Domain.Board;
+
+namespace Solver.Domain.Tests.Board;
+
+public class BoardSolverTests
+{
+    [Test]
+    public void File1IsSolved()
+    {
+        var builder = new BoardBuilder();
+        var b = builder.CreateFrom9x9Csv(@"Unsolved\input.txt");;
+        var solver = new BoardSolver();
+        var solution = solver.GetSolvedBoard(b);
+        
+        Assert.IsTrue(solution.IsValid);
+        Assert.IsTrue(solution.IsSolved);
+    }
+}
