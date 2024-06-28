f4a447c89bf583c8b4801d63a24f1f27196cf719
zero <squiredd@gmail.com>
2024-06-28 11:15:14 -0700

    add permutate

---
+++ source/Solver.Domain/SystemCollections/EnumerableExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace Solver.Domain.SystemCollections;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<IReadOnlyList<T>> Permutate<T>(this IEnumerable<T> items, int count)
+    {
+        if (count < 1) yield break;
+        if(count == 1)
+        {
+            foreach (var x in items.Select(x => new T[] {x}))
+            {
+                yield return x;
+            }
+            yield break;
+        }
+
+        var itemsArray = items as T[] ?? items.ToArray();
+        if (itemsArray.Length == 0 || itemsArray.Length < count)
+        {
+            yield break;
+        }
+        if (count >= itemsArray.Length)
+        {
+            yield return itemsArray;
+            yield break;
+        }
+        var first = itemsArray.First();
+        var rest = new Queue<T>(itemsArray.Skip(1));
+
+        do
+        {
+            foreach (var window in rest.Permutate(count-1))
+            {
+                yield return window.Prepend(first).ToArray();
+            }
+
+            first = rest.Dequeue();
+        } while (rest.Count> count-2);
+        
+    }
+}
