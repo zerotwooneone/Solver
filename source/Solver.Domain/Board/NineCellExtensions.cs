e9009473eb18afa6512634dbbf6965aca6b14201
zero <squiredd@gmail.com>
2024-06-29 16:46:53 -0700

    cleanup

--- source/Solver.Domain/Board/NineCellExtensions.cs
+++ source/Solver.Domain/Board/NineCellExtensions.cs
@@ -11,7 +11,7 @@ public static class NineCellExtensions
     /// <returns></returns>
     public static (bool IsValid, bool IsSolved) Check(this IEnumerable<ICell>? cells)
     {
-        if(cells == null)
+        if (cells == null)
         {
             return (false, false);
         }
@@ -20,11 +20,12 @@ public static class NineCellExtensions
         var count = 0;
         foreach (var cell in cells)
         {
-            if (cell.Value ==null)
+            if (cell.Value == null)
             {
                 continue;
             }
-            if(!hash.Add(cell))
+
+            if (!hash.Add(cell))
             {
                 return (false, false);
             }
@@ -36,7 +37,9 @@ public static class NineCellExtensions
         {
             return (false, false);
         }
-        return (true, hash.Count == 9);;
+
+        return (true, hash.Count == 9);
+        ;
     }
 
     public static IEnumerable<ICell> GetValues(this IEnumerable<ICell>? cells)
@@ -48,7 +51,7 @@ public static class NineCellExtensions
 
         return cells.Where(c => c.Value != null);
     }
-    
+
     public static (HashSet<CellValue> remaining, HashSet<CellValue> solved) GetStats(this IReadOnlyList<ICell> cells)
     {
         var existing = cells.Aggregate(new HashSet<CellValue>(), (hashSet, cell) =>
@@ -57,15 +60,16 @@ public static class NineCellExtensions
             {
                 hashSet.Add(cell.Value.Value);
             }
+
             return hashSet;
         });
-        var remaining = new HashSet<CellValue>( CellValue.AllValues.Where(c=>!existing.Contains(c)));
+        var remaining = new HashSet<CellValue>(CellValue.AllValues.Where(c => !existing.Contains(c)));
         return (remaining, existing);
     }
 
     internal static MutableNineCell GetMutableNineCell(this IReadOnlyCollection<ICell> cells)
     {
-        if(cells is MutableNineCell mutable)
+        if (cells is MutableNineCell mutable)
         {
             return mutable;
         }
@@ -74,55 +78,62 @@ public static class NineCellExtensions
         {
             throw new ArgumentException("NineCell must have exactly 9 cells", nameof(cells));
         }
+
         return new MutableNineCell(GetMutableCells(cells));
     }
-    
+
     internal static IReadOnlyCollection<MutableCell> GetMutableCells(this IReadOnlyCollection<ICell> cells)
     {
-        return cells.Select((c,i)=>new MutableCell(i, c.Value, c.State.RemainingValues)).ToArray();
+        return cells.Select((c, i) => new MutableCell(i, c.Value, c.State.RemainingValues)).ToArray();
     }
 
     public static bool TryGetHiddenPair(
         this IReadOnlyList<MutableCell> cells,
         out (MutableCell one, MutableCell two, CellValue value1, CellValue value2)? result)
     {
-        //var knownRemaining = new(int index, HashSet <CellValue> uniqueRemaining)[9];
-        for (int firstIndex = 0; firstIndex < cells.Count-1; firstIndex++)
+        for (var firstIndex = 0; firstIndex < cells.Count - 1; firstIndex++)
         {
             var first = cells[firstIndex];
-            if (first.Value.HasValue || first.State.RemainingValues.Count ==1)
+            if (first.Value.HasValue || first.State.RemainingValues.Count == 1)
             {
                 continue;
             }
-            for (int secondIndex = firstIndex + 1; secondIndex < cells.Count; secondIndex++)
+
+            for (var secondIndex = firstIndex + 1; secondIndex < cells.Count; secondIndex++)
             {
                 var second = cells[secondIndex];
-                if (second.Value.HasValue || second.State.RemainingValues.Count ==1)
+                if (second.Value.HasValue || second.State.RemainingValues.Count == 1)
                 {
                     continue;
                 }
-                
-                var intersect = first.State.RemainingValues.Values.Intersect(second.State.RemainingValues.Values).ToArray();
+
+                var intersect = first.State.RemainingValues.Values
+                    .Intersect(second.State.RemainingValues.Values)
+                    .ToArray();
                 if (intersect.Length < 2)
                 {
                     continue;
                 }
-                
-                var others = cells.Where(c=>!c.Value.HasValue && c.Index!=firstIndex && c.Index!=secondIndex).ToArray();
-                if (!others.Any())
+
+                var others = cells
+                    .Where(c => !c.Value.HasValue && c.Index != firstIndex && c.Index != secondIndex)
+                    .ToArray();
+                if (others.Length == 0)
                 {
                     continue;
                 }
 
                 var otherRemainingValues = others
-                    .Select(m=>m.State.RemainingValues.Values);
-                var otherIntersect = otherRemainingValues.Skip(1).Aggregate((IEnumerable<CellValue>)otherRemainingValues.First(), (a,b)=>
-                {
-                    return a.Intersect(b);
-                });
+                    .Select(m => m.State.RemainingValues.Values)
+                    .ToArray();
+                var otherIntersect = otherRemainingValues
+                    .Skip(1)
+                    .Aggregate((IEnumerable<CellValue>) otherRemainingValues.First(), (a, b) => a.Intersect(b));
                 var otherHash = new HashSet<CellValue>(otherIntersect);
 
-                var distinctIntersect = intersect.Where(i => !otherHash.Contains(i)).ToArray();
+                var distinctIntersect = intersect
+                    .Where(i => !otherHash.Contains(i))
+                    .ToArray();
                 if (distinctIntersect.Length != 2)
                 {
                     continue;
@@ -133,25 +144,6 @@ public static class NineCellExtensions
             }
         }
 
-        /*var knownRemaining = new HashSet<CellValue>(CellValue.AllValues);
-        var remainingDistinctByCellValue = new Dictionary<CellValue, List<MutableCell>>();
-        foreach (var cell in cells)
-        {
-            if (cell.Value.HasValue)
-            {
-                knownRemaining.Remove(cell.Value.Value);
-                continue;
-            }
-
-            var didRemove = new List<CellValue>();
-            foreach (var value in cell.RemainingCellValues)
-            {
-                if (knownRemaining.Remove(value))
-                {
-                    didRemove.Add(value);
-                }
-            }
-        }*/
         result = null;
         return false;
     }
