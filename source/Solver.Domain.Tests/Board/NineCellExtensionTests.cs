0a55e0d274ad946165087ccbd795c152120d1c01
zero <squiredd@gmail.com>
2024-06-29 20:18:08 -0700

    add triple test

--- source/Solver.Domain.Tests/Board/NineCellExtensionTests.cs
+++ source/Solver.Domain.Tests/Board/NineCellExtensionTests.cs
@@ -6,7 +6,7 @@ namespace Solver.Domain.Tests.Board;
 public class NineCellExtensionTests
 {
     [Test]
-    public void GetHiddenPair_PairExists_AndFound()
+    public void GetHidden_PairExists_AndFound()
     {
         var cells = new []{
             new MutableCell( null, new CellValue[]{3,4,7,8,9}),
@@ -29,6 +29,34 @@ public class NineCellExtensionTests
         var pair = tuple.Value.pair;
         var expected = new CellValue[]{2,6};
         var actual = new[]{pair.Value.value1, pair.Value.value2};
+
+        CollectionAssert.AreEquivalent(expected, actual);
+    }
+    
+    [Test]
+    public void GetHidden_TripleExists_AndFound()
+    {
+        var cells = new []{
+            new MutableCell( 1, Array.Empty<CellValue>()),
+            new MutableCell( 3, Array.Empty<CellValue>()),
+            new MutableCell( null, new CellValue[]{2}),
+            new MutableCell( null, new CellValue[]{2,4,5,6,7,8}),
+            new MutableCell( 9, Array.Empty<CellValue>()),
+            new MutableCell( null, new CellValue[]{2,4,8}),
+            new MutableCell( null, new CellValue[]{2,4,5,6,7,8}),
+            new MutableCell( null, new CellValue[]{2,4,5,6,7,8}),
+            new MutableCell( null, new CellValue[]{2,4,8}),
+        };
+        
+        Assert.IsTrue(cells.Check().IsValid);
+        Assert.IsFalse(cells.Check().IsSolved);
+
+        Assert.IsTrue(cells.TryGetHidden(out var tuple));
+        Assert.NotNull(tuple);
+        
+        var triple = tuple.Value.triple;
+        var expected = new CellValue[]{5,6,7};
+        var actual = new[]{triple.Value.value1, triple.Value.value2, triple.Value.value3};
 
         CollectionAssert.AreEquivalent(expected, actual);
     }
